e71ffc3e24d1c75eab97e303f08d3bad484d6a3d
Sebastien Arbogast <sebastien.arbogast@gmail.com>
2012-04-15 15:36:50 +0200

    Added license and metadata

--- BootstrapFileUploadGrailsPlugin.groovy
+++ BootstrapFileUploadGrailsPlugin.groovy
@@ -1,21 +1,40 @@
+/*
+ * Copyright 2012 Sebastien Arbogast
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 class BootstrapFileUploadGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "1.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/views/error.gsp"
+            "grails-app/views/error.gsp",
+            "grails-app/views/index.gsp",
+            "grails-app/views/postmessage.gsp",
+            "grails-app/views/result.gsp"
     ]
 
     // TODO Fill in these fields
     def title = "Bootstrap File Upload Plugin" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
+    def author = "Sebastien Arbogast"
+    def authorEmail = "sebastien.arbogast@epseelon.com"
     def description = '''\
-Brief summary/description of the plugin.
+Grails Plugin to integrate Sebastian Tschan's Jquery File Upload (https://github.com/blueimp/jQuery-File-Upload).
 '''
 
     // URL to the plugin's documentation
@@ -24,19 +43,19 @@ Brief summary/description of the plugin.
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+    def organization = [ name: "Epseelon", url: "http://www.epseelon.com/" ]
 
     // Any additional developers beyond the author specified above.
 //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "GITHUB", url: "https://github.com/sarbogast/grails-bootstrap-file-upload/issues" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
+    def scm = [ url: "https://github.com/sarbogast/grails-bootstrap-file-upload" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
